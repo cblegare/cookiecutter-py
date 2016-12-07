5bfb62dbb3390109ff46bccca6a8dfda7ce668bd
Charles Bouchard-Légaré <cblegare.atl@ntis.ca>
2016-12-06 21:52:41 -0500

    Added minimal flask views and tests

--- {{cookiecutter.project_slug}}/tests/functional/test_web.py
+++ {{cookiecutter.project_slug}}/tests/functional/test_web.py
@@ -17,5 +17,36 @@ def app():
     return app
 
 
-def test_app(app):
-    assert isinstance(app, flask.Flask)
+@pytest.fixture
+def client(app):
+    new_client = app.test_client()
+    return new_client
+
+
+def test_web_get_root_is_ok(client):
+    rv = client.get('/')
+    assert rv.status_code == 200, 'GET root route '
+
+
+def test_web_get_greetings_is_ok(client):
+    rv = client.get('/greetings')
+    assert rv.status_code == 404, 'GET greetings without arg does not exists'
+
+
+def test_web_get_greetings_with_name(client):
+    rv = client.get('/greetings/John')
+    assert rv.status_code == 200, 'GET greetings with arg responds "OK"'
+    assert 'Hello John!' in rv.data.decode(), 'greetings responded as expected'
+
+
+def test_web_get_greetings_with_name_and_formal_style(client):
+    rv = client.get('/greetings/John?style=formal')
+    assert rv.status_code == 200, 'GET greetings with arg responds "OK"'
+    assert 'Good evening John' in rv.data.decode(), (''
+        'greetings responded as expected')
+
+
+def test_web_get_greetings_with_name_and_slang_style(client):
+    rv = client.get('/greetings/John?style=slang')
+    assert rv.status_code == 200, 'GET greetings with arg responds "OK"'
+    assert 'Yo !!1 John!' in rv.data.decode(), 'greetings responded as expected'
--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/web.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/web.py
@@ -4,10 +4,39 @@
 
 """Cookiecutter template tests."""
 
-from flask import Flask
+from flask import Blueprint, Flask, request
+
+from {% if cookiecutter.namespace %}{{ cookiecutter.namespace }}.{{ cookiecutter.project_slug }}{% else %}{{ cookiecutter.project_slug }}{% endif %} import greetings
+
+{{ cookiecutter.project_slug }} = Blueprint('greeting_page', __name__)
+
+all_blueprints = [{{ cookiecutter.project_slug }}]
+
+
+@{{ cookiecutter.project_slug }}.route('/')
+def root_view():
+    """Trivial root resource."""
+    return ''
+
+
+@{{ cookiecutter.project_slug }}.route('/greetings/<person>')
+def greetings_view(person='World!'):
+    """Provide the greeting resource."""
+    style = request.args.get('style', None)
+
+    if style == 'slang':
+        message = str(greetings.Slang(person))
+    elif style == 'formal':
+        message = str(greetings.Formal(person))
+    else:
+        message = str(greetings.Greetings(person))
+
+    return message
 
 
 def create_app():
     """Create a new Flask web application and returns it."""
     app = Flask(__name__)
+    for blueprint in all_blueprints:
+        app.register_blueprint(blueprint)
     return app
